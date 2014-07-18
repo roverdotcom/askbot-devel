4104445a1fac89c2505aa3a84cc9cdb3dc21fd7e
geekofalltrades <thegeekofalltrades@gmail.com>
2014-07-18 11:16:48 -0700

    [DEV-1152] Add __init__ method to AskbotUser class.

--- askbot/models/__init__.py
+++ askbot/models/__init__.py
@@ -76,6 +76,12 @@ class AskbotUser(models.Model):
     class Meta(object):
         app_label = 'askbot'
 
+    def __init__(self, *args, **kwargs):
+        """Create and save underlying User before initializing AskbotUser."""
+        # TO DO: resolve need for an existing User id. Delve into Askbot to
+        # figure out how it creates its users.
+        super(AskbotUser, self).__init__(*args, **kwargs)
+
     def save(self, *args, **kwargs):
         """Save self.user prior to saving self."""
         self.user.save()
