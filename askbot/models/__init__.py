7ddea20d9b02cda834400b900b91a8076939066a
Matt Dougherty <thegeekofalltrades@gmail.com>
2014-07-21 09:29:59 -0700

    [DEV-1152] Move AskbotUser into separate askbot_user.py.

--- askbot/models/__init__.py
+++ askbot/models/__init__.py
@@ -64,37 +64,8 @@ from askbot import mail
 from askbot.models import signals
 
 
-from django.contrib.auth.models import User as DjangoUser
+from askbot_user import User
 
-
-class AskbotUser(models.Model):
-    """Custom user model which encapsulates askbot functionality.
-    Replaces monkey-patched auth User model.
-    """
-    user = models.OneToOneField(DjangoUser)
-
-    class Meta(object):
-        app_label = 'askbot'
-
-    def __init__(self, *args, **kwargs):
-        """Create and save underlying User before initializing AskbotUser."""
-        # TO DO: resolve need for an existing User id. Delve into Askbot to
-        # figure out how it creates its users.
-        super(AskbotUser, self).__init__(*args, **kwargs)
-
-    def __getattr__(self, name):
-        """If the AskbotUser does not have some attribute, look for it on the
-        AskbotUser's User object.
-        """
-        return getattr(self.user, name)
-
-    def save(self, *args, **kwargs):
-        """Save self.user prior to saving self."""
-        self.user.save()
-        super(AskbotUser, self).save(*args, **kwargs)
-
-
-User = AskbotUser
 
 # set up a possibility for the users to follow others
 try:
