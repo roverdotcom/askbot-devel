4f810100a9d51d850e01e0e51f68ceece040e214
Matt Dougherty <thegeekofalltrades@gmail.com>
2014-08-20 16:09:48 -0700

    [DEV-1471] Add check for askbot url to middleware.

--- askbot/middleware/feature_flag.py
+++ askbot/middleware/feature_flag.py
@@ -4,10 +4,13 @@ page if Askbot's feature flag is switched off.
 from gargoyle import gargoyle
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.conf import settings as django_settings
 
 
 class AskbotFeatureFlagMiddleware(object):
     """Redirect Askbot urls to root if the flag is disabled."""
     def process_request(self, request):
-        if not request.user.is_staff and not gargoyle.is_active('askrover'):
+        if request.path.startswith('/' + django_settings.ASKBOT_URL) and \
+                not request.user.is_staff and \
+                not gargoyle.is_active('askrover'):
             return HttpResponseRedirect(reverse('index'))
