9f6c70fc682b1d6dc284bd90314ab0d8697b6386
Matt Dougherty <thegeekofalltrades@gmail.com>
2014-09-30 16:21:59 -0700

    [DEV-1739_session] Rebrand askbot_user views to askrover. Implement skeleton views for most recent question or answer by a given user.

--- askbot/views/askrover.py
+++ askbot/views/askrover.py
@@ -1,9 +1,16 @@
 from django.views.generic.base import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.utils import simplejson
+from django.shortcuts import get_object_or_404
 from askbot.models import User
+from askbot.models import AnonymousAnswer
+from askbot.models import AnonymousQuestion
+from askbot.models import Post
+from askbot.models import Answer
 
 
 class FollowUser(View):
@@ -55,3 +62,27 @@ class UnfollowUser(View):
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(UnfollowUser, self).dispatch(*args, **kwargs)
+
+
+class MostRecentQuestion(View):
+    """Redirects to the most recent question posted by the given user.
+
+    Fails through to the list view, and can be directed to fail through
+    to the list view via a GET parameter if a question wasn't just posted
+    anonymously.
+    """
+
+    def get(self, request, id=None, **kwargs):
+        pass
+
+
+class MostRecentAnswer(View):
+    """Redirects to the most recent answer posted by the given user.
+
+    Fails through to the list view, and can be directed to fail through
+    to the list view via a GET parameter if an answer wasn't just posted
+    anonymously.
+    """
+
+    def get(self, request, id=None, **kwargs):
+        pass
