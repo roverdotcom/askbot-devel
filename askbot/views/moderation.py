3c0d27c2dbb2e604b0f583f1500613e323a5dcd9
Mike Hansen <mike@rover.com>
2015-03-03 10:25:46 -0800

    [WIZ-1502] Fix request.raw_post_data which was deprecated in Django 1.6

--- askbot/views/moderation.py
+++ askbot/views/moderation.py
@@ -55,7 +55,7 @@ def moderate_post_edits(request):
     if not request.user.is_administrator_or_moderator():
         raise exceptions.PermissionDenied()
 
-    post_data = json.loads(request.raw_post_data)
+    post_data = json.loads(request.body)
     #{'action': 'decline-with-reason', 'items': ['posts'], 'reason': 1, 'edit_ids': [827]}
 
     memo_set = models.ActivityAuditStatus.objects.filter(
