63e9bb58b93d47fb75d7c7ad4d045b580a99c778
John Williams <john@rover.com>
2015-05-26 14:52:39 -0700

    Pin all requirements

--- askbot/__init__.py
+++ askbot/__init__.py
@@ -12,28 +12,27 @@ VERSION = (0, 7, 49)
 #keys are module names used by python imports,
 #values - the package qualifier to use for pip
 REQUIREMENTS = {
-    'akismet': 'akismet',
+    'akismet': 'akismet==0.2.0',
     'django': 'django>=1.3.1,<1.7',
-    'jinja2': 'Jinja2',
+    'jinja2': 'Jinja2==2.7.3',
     'coffin': 'Coffin>=0.3,<2.0',
     'south': 'South>=0.7.1',
-    'oauth2': 'oauth2',
-    'markdown2': 'markdown2',
-    'keyedcache': 'django-keyedcache',
+    'oauth2': 'oauth2==1.5.211',
+    'markdown2': 'markdown2==2.3.0',
+    'keyedcache': 'django-keyedcache==1.5.1',
     'threaded_multihost': 'django-threaded-multihost',
-    'robots': 'django-robots',
+    'robots': 'django-robots==1.0',
     'sanction': 'sanction==0.3.1',
-    'unidecode': 'unidecode',
     'django_countries': 'django-countries==1.0.5',
     'djcelery': 'django-celery>=3.0.11',
     'djkombu': 'django-kombu==0.9.4',
     'recaptcha_works': 'django-recaptcha-works',
-    'openid': 'python-openid',
+    'openid': 'python-openid==2.2.5',
     'pystache': 'pystache>=0.5.0',
     'tinymce': 'django-tinymce==1.5.3',
-    'bs4': 'beautifulsoup4',
+    'bs4': 'beautifulsoup4==4.3.2',
     'picklefield': 'django-picklefield==0.3.1',
-    'longerusername': 'longerusername',
+    'longerusername': 'longerusername==0.4',
     #'stopforumspam': 'stopforumspam'
 }
 
@@ -42,6 +41,7 @@ EXTERNALLY_SATISFIED_REQUIREMENTS = {
     'pytz': 'pytz==2013b',
     'html5lib': 'html5lib==0.90',
     'compressor': 'django-compressor==1.2',
+    'unidecode': 'unidecode==0.4.14',
 }
 
 if platform.system() != 'Windows':
