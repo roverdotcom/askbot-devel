e4a84892f1757fe5aa29c983a31082fc50b7b969
Ailenei Dan <dan.ailenei@rover.com>
2019-12-13 15:10:07 +0200

    remove cached questions

--- askbot/models/question.py
+++ askbot/models/question.py
@@ -1672,10 +1672,7 @@ class Thread(models.Model):
         return last_updated_at, last_updated_by
 
     def get_summary_html(self, search_state=None, visitor=None):
-        html = (
-            self.get_cached_summary_html(visitor) or
-            self.update_summary_html(visitor)
-        )
+        html = self.update_summary_html(visitor)
 
         # TODO: this work may be pushed onto javascript we post-process tag names
         # in the snippet so that tag urls match the search state
